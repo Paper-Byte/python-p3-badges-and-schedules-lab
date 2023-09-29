f1c394aac9501d082b92c1bcdb0177480acc9a7e
Noah <noahhughesph@gmail.com>
2023-09-29 00:57:53 -0400

    all tests passing

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,25 @@
 def badge_maker(name):
-    return None
+    return str(f'Hello, my name is {name}.')
+
 
 def batch_badge_creator(names):
-    return None
+    return [(f'Hello, my name is {name}.') for name in names]
+
 
 def assign_rooms(names):
-    return None
+    assignmeant = []
+    room = 1
+    for name in names:
+        assignmeant.append(
+            f"Hello, {name}! You'll be assigned to room {room}!")
+        room += 1
+    return assignmeant
+
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    rooms = assign_rooms(names)
+    for badge in badges:
+        print(badge)
+    for room in rooms:
+        print(room)
